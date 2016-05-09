54c9a24c55aa8a43d8653b5467a3eb6255bd10bb
puldavid87 <puldavid87@gmail.com>
2016-05-09 17:07:17 -0500

    calculadora solo suma

--- CALCULADORA/CALCULADORA/CALCULADORA.ino
+++ CALCULADORA/CALCULADORA/CALCULADORA.ino
@@ -1,6 +1,7 @@
 String inString="";
 int num1;
 int num2;
+int total;
 int i=0;
 int oper;
 void setup() {
@@ -15,17 +16,26 @@ void loop() {
       switch(i){
         case 0:
             num1=inString.toInt();
-            Serial.println(num1);
+            Serial.println("INGRESE OPERANDO");
             i++;
-            Serial.println(i);
         break;
         case 1:
           if(inString=="+"){
-              Serial.println("SUMAR");
               oper=1;
+              Serial.println("INGRESE 2 NUM");
            }
             i++;
         break;
+        case 2:
+        num2=inString.toInt();
+        if(oper==1){
+          total=num1+num2;
+          Serial.print("Resultado:");
+          Serial.println(total);
+        }
+        i=0;
+        break;
+        
         }
     }  
   
